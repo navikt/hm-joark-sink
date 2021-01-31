b48dc553bc4d86721c358b4ff182bfae88a1336a
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2021-01-31 14:27:55 +0100

    tar inn onError for å forstå hvorfor intet skjer

--- src/main/kotlin/no/nav/hjelpemidler/joark/service/JoarkDataSink.kt
+++ src/main/kotlin/no/nav/hjelpemidler/joark/service/JoarkDataSink.kt
@@ -72,6 +72,11 @@ internal class JoarkDataSink(
         }
     }
 
+    override fun onError(problems: MessageProblems, context: RapidsConnection.MessageContext) {
+        super.onError(problems, context)
+        logger.info(problems.toExtendedReport())
+    }
+
     private suspend fun genererPdf(soknadJson: String, soknadId: UUID) =
         kotlin.runCatching {
             pdfClient.genererPdf(soknadJson)
