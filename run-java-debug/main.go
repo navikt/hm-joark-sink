67a3198311562febb2df83fa4c58bf35800dec50
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2021-04-27 09:55:34 +0200

    Added http server for isready/isalive to avoid pod being restarted by kubernetes

--- run-java-debug/main.go
+++ run-java-debug/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
     "fmt"
+    "io"
     "os"
     "os/exec"
     "time"
+    "net/http"
 )
 
 func main() {
@@ -12,6 +14,16 @@ func main() {
 
     out, err := exec.Command("/usr/local/openjdk-15/bin/java", os.Args[1:]...).Output()
     fmt.Printf("Java application exit: err=%#v\nerrStr=%q\ndata:\n%s\n", err, err, out)
+
+    go func() {
+        http.HandleFunc("/isready", func(w http.ResponseWriter, r *http.Request) {
+            io.WriteString(w, "READY")
+        })
+        http.HandleFunc("/isalive", func(w http.ResponseWriter, r *http.Request) {
+            io.WriteString(w, "ALIVE")
+        })
+        fmt.Println(http.ListenAndServe(":8080", nil))
+    }()
 
     fmt.Println("Sleeping for an hour to allow you to collect the output: /oom-dump.hprof")
     time.Sleep(time.Hour)
