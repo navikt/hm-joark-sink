0560f3ef6ef906e10a523e77559a2a22b28ea3c5
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2021-04-27 09:31:50 +0200

    Updated logging to find bug

--- run-java-debug/main.go
+++ run-java-debug/main.go
@@ -8,10 +8,10 @@ import (
 )
 
 func main() {
-    fmt.Println("Starting up JAVA project:")
+    fmt.Printf("Starting up JAVA project, arguments: %#v\n", os.Args)
 
     err := exec.Command("/usr/local/openjdk-15/bin/java", os.Args[1:]...).Run()
-    fmt.Printf("Java application exit: err=%#v\n", err)
+    fmt.Printf("Java application exit: err=%#v\nerrStr=%q\n", err, err)
 
     fmt.Println("Sleeping for an hour to allow you to collect the output: /oom-dump.hprof")
     time.Sleep(time.Hour)
