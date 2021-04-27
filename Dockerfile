0a4993f9bd7459726d7d7808ca2ca11f711a5401
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2021-04-27 09:06:06 +0200

    Attempt at adding custom run-java.sh that keeps the container going after dumping heap mem.

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,5 @@ COPY build/libs/hm-joark-sink-all.jar app.jar
 ENV JAVA_OPTS="-XX:MaxRAMPercentage=75 \
                -XX:+HeapDumpOnOutOfMemoryError \
                -XX:HeapDumpPath=/oom-dump.hprof"
+
+COPY run-java-debug.sh /run-java.sh
---
+++ run-java-debug.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if test -d "/app/WEB-INF";
+then
+    set -x
+    exec java \
+    ${DEFAULT_JVM_OPTS} \
+    ${JAVA_OPTS} \
+    -server \
+    -classpath \
+    ${CLASSPATH} \
+    ${MAIN_CLASS} \
+    ${RUNTIME_OPTS} \
+    $@
+else
+    set -x
+    exec bash -c "java ${DEFAULT_JVM_OPTS} ${JAVA_OPTS} -jar ${APP_JAR} ${RUNTIME_OPTS} $@ || sleep 3600"
+fi
