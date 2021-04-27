a0000db0bf748507e072368a1ae662db92ec2ee5
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2021-04-27 09:44:29 +0200

    Log java process output

--- run-java-debug/main.go
+++ run-java-debug/main.go
@@ -10,8 +10,8 @@ import (
 func main() {
     fmt.Printf("Starting up JAVA project, arguments: %#v\n", os.Args)
 
-    err := exec.Command("/usr/local/openjdk-15/bin/java", os.Args[1:]...).Run()
-    fmt.Printf("Java application exit: err=%#v\nerrStr=%q\n", err, err)
+    out, err := exec.Command("/usr/local/openjdk-15/bin/java", os.Args[1:]...).Output()
+    fmt.Printf("Java application exit: err=%#v\nerrStr=%q\ndata:\n%s\n", err, err, out)
 
     fmt.Println("Sleeping for an hour to allow you to collect the output: /oom-dump.hprof")
     time.Sleep(time.Hour)
