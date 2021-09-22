9f8ee3dbfaed74e6c766c500b69cd7ad0edb68c0
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2021-09-22 10:23:00 +0200

    Koblar inn ny river

--- src/main/kotlin/no/nav/hjelpemidler/joark/Application.kt
+++ src/main/kotlin/no/nav/hjelpemidler/joark/Application.kt
@@ -3,8 +3,10 @@ package no.nav.hjelpemidler.joark
 import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.hjelpemidler.joark.joark.AzureClient
 import no.nav.hjelpemidler.joark.joark.JoarkClient
+import no.nav.hjelpemidler.joark.joark.JoarkClientV2
 import no.nav.hjelpemidler.joark.pdf.PdfClient
 import no.nav.hjelpemidler.joark.service.JoarkDataSink
+import no.nav.hjelpemidler.joark.service.OpprettMidlertidigJournalpost
 import no.nav.hjelpemidler.joark.wiremock.WiremockServer
 
 fun main() {
@@ -24,9 +26,15 @@ fun main() {
         accesstokenScope = Configuration.joark.joarkScope,
         azureClient = azureClient
     )
+    val joarkClientv2 = JoarkClientV2(
+        baseUrl = Configuration.joark.baseUrl,
+        accesstokenScope = Configuration.joark.joarkScope,
+        azureClient = azureClient
+    )
 
     RapidApplication.Builder(RapidApplication.RapidApplicationConfig.fromEnv(Configuration.rapidApplication))
         .build().apply {
             JoarkDataSink(this, pdfClient, joarkClient)
+            OpprettMidlertidigJournalpost(this, pdfClient, joarkClientv2)
         }.start()
 }
