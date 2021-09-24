7cff6a0ef3405978d98a2624d0a99060b1366e87
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2021-09-24 12:12:19 +0200

    logger om vi er i dev

--- src/main/kotlin/no/nav/hjelpemidler/joark/Application.kt
+++ src/main/kotlin/no/nav/hjelpemidler/joark/Application.kt
@@ -1,13 +1,15 @@
 package no.nav.hjelpemidler.joark
 
+import mu.KotlinLogging
 import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.hjelpemidler.joark.joark.AzureClient
 import no.nav.hjelpemidler.joark.joark.JoarkClient
 import no.nav.hjelpemidler.joark.joark.JoarkClientV2
 import no.nav.hjelpemidler.joark.pdf.PdfClient
 import no.nav.hjelpemidler.joark.service.JoarkDataSink
-import no.nav.hjelpemidler.joark.service.OpprettMidlertidigJournalpost
 import no.nav.hjelpemidler.joark.wiremock.WiremockServer
+
+private val logger = KotlinLogging.logger {}
 
 fun main() {
 
@@ -35,6 +37,10 @@ fun main() {
     RapidApplication.Builder(RapidApplication.RapidApplicationConfig.fromEnv(Configuration.rapidApplication))
         .build().apply {
             JoarkDataSink(this, pdfClient, joarkClient)
-            //OpprettMidlertidigJournalpost(this, pdfClient, joarkClientv2)
+            if (System.getenv("NAIS_CLUSTER_NAME") != null && System.getenv("NAIS_CLUSTER_NAME") == "dev-gcp") {
+                logger.info { "Vi er i dev-gcp" }
+                logger.info { "Cluster name: " + System.getenv("NAIS_CLUSTER_NAME") }
+                // OpprettMidlertidigJournalpost(this, pdfClient, joarkClientv2)
+            }
         }.start()
 }
