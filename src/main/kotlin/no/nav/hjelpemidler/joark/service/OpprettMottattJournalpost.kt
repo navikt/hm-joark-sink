af8594b9ed2f63b5542a96532482b970895e59f5
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2021-10-14 10:34:18 +0200

    Endrer eksternRefId

--- src/main/kotlin/no/nav/hjelpemidler/joark/service/OpprettMottattJournalpost.kt
+++ src/main/kotlin/no/nav/hjelpemidler/joark/service/OpprettMottattJournalpost.kt
@@ -111,7 +111,7 @@ internal class OpprettMottattJournalpost(
         soknadPdf: ByteArray
     ) =
         kotlin.runCatching {
-            joarkClient.arkiverSoknad(fnrBruker, navnAvsender, soknadId, soknadPdf, soknadId.toString() + "HOTSAK_TIL_OEBS")
+            joarkClient.arkiverSoknad(fnrBruker, navnAvsender, soknadId, soknadPdf, soknadId.toString() + "HOTSAK_TIL_GOSYS")
         }.onSuccess {
             val journalpostnr = it
             logger.info("Opprettet journalpost med status mottatt i joark, journalpostNr: $journalpostnr")
