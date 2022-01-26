07cb752113093d0e00d8b7142eddc347106334f2
matsbyfl <mats@byfuglien.net>
2022-01-26 09:31:32 +0100

    Legger til feilende feilregistrert journalpost i skiplist

--- src/main/kotlin/no/nav/hjelpemidler/joark/service/FeilregistrerFerdigstiltJournalpost.kt
+++ src/main/kotlin/no/nav/hjelpemidler/joark/service/FeilregistrerFerdigstiltJournalpost.kt
@@ -58,7 +58,7 @@ internal class FeilregistrerFerdigstiltJournalpost(
 
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
 
-        if (packet.journalpostId == "510871046") {
+        if (packet.journalpostId == "535250492") {
             return
         }
 
