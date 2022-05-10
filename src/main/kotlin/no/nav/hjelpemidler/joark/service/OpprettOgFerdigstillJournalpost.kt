66ce90bbcc7f7f7b30b22c410fb25b57ab6a30a1
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2022-05-10 11:59:21 +0200

    Removed skip code again

--- src/main/kotlin/no/nav/hjelpemidler/joark/service/OpprettOgFerdigstillJournalpost.kt
+++ src/main/kotlin/no/nav/hjelpemidler/joark/service/OpprettOgFerdigstillJournalpost.kt
@@ -64,13 +64,6 @@ internal class OpprettOgFerdigstillJournalpost(
     }
 
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
-        // FIXME: Remove again, temporary skipping of event
-        if (packet.søknadId == "aa63c5a9-4c51-4979-82b7-c435ffca55fb") {
-            // 409 conflict.
-            logger.info("Skipping event with sakId=${packet.sakId} and soknadsId=${packet.søknadId}")
-            return
-        }
-
         runBlocking {
             withContext(Dispatchers.IO) {
                 launch {
