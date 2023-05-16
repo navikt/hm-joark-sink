c7234c2f532f3920b9d73ebc1dad442a538ff9b8
Erik Eiesland <erik@eies.land>
2023-05-16 10:38:32 +0200

    ny versjon av hm-http

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     implementation("io.github.microutils:kotlin-logging:3.0.5")
 
     // Http
-    implementation("no.nav.hjelpemidler.http:hm-http:v0.0.33")
+    implementation("no.nav.hjelpemidler.http:hm-http:v0.0.35")
 
     // Testing
     testImplementation(kotlin("test"))
