855f4cd949c2eeac100ee5eb2c389c2541f1ed61
Erik Eiesland <erik@eies.land>
2023-05-16 13:29:46 +0200

    fjerner logging av body

--- src/main/kotlin/no/nav/hjelpemidler/dokarkiv/DokarkivClient.kt
+++ src/main/kotlin/no/nav/hjelpemidler/dokarkiv/DokarkivClient.kt
@@ -62,7 +62,7 @@ class DokarkivClient(
                     secureLog.info(message)
                 }
             }
-            level = LogLevel.ALL
+            level = LogLevel.HEADERS
         }
     }
 
