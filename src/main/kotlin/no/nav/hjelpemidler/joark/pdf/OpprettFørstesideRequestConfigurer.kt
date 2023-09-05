3ed313dfc2e8c764fa35d58daf12c75b5fc8f9cb
matsbyfl <mats@byfuglien.net>
2023-09-05 12:50:27 +0200

    Setter arkivtittel når førsteside genereres for å hindre at ettersendte dokumenter kommer inn med blank tittel

--- src/main/kotlin/no/nav/hjelpemidler/joark/pdf/OpprettFørstesideRequestConfigurer.kt
+++ src/main/kotlin/no/nav/hjelpemidler/joark/pdf/OpprettFørstesideRequestConfigurer.kt
@@ -14,6 +14,7 @@ class OpprettFørstesideRequestConfigurer(
     var språkkode: Språkkode = Språkkode.NB
     var adresse: Adresse = defaultAdresse
     var brevkode: String? = null
+    var arkivtittel: String? = tittel // TODO Midlertidig hardkodet til problemene i hm-saksbehandling er løst. På grunn av litt ustabil oppførsel, er det litt risikabelt å prodsette nye endringer på hm-saksbehandling. Når det er løst, kan arkivtittel leses fra et eget felt i kafka meldingen
     var enhetsnummer: String? = null
 
     operator fun invoke(): PostFoerstesideRequest = OpprettFørstesideRequest(
@@ -26,7 +27,7 @@ class OpprettFørstesideRequestConfigurer(
         bruker = Bruker(fnrBruker, Bruker.BrukerType.PERSON),
         tema = Tema.HJE.toString(),
         behandlingstema = null,
-        arkivtittel = null,
+        arkivtittel = arkivtittel,
         vedleggsliste = vedlegg[språkkode],
         navSkjemaId = brevkode,
         dokumentlisteFoersteside = vedlegg[språkkode],
