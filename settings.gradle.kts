05e9aaceb9f0ea582c765f13dd2948ff4ea10408
Erik Eiesland <di0nys1us@users.noreply.github.com>
2024-02-20 11:54:41 +0100

    fjerner maven("https://jitpack.io")

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,6 @@ dependencyResolutionManagement {
     @Suppress("UnstableApiUsage")
     repositories {
         mavenCentral()
-        maven("https://jitpack.io")
         maven {
             url = uri("https://maven.pkg.github.com/navikt/*")
             credentials {
