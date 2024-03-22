507b5567682c96fa705b98d79a1ff7382a854698
Erik Eiesland <erik.eiesland@nav.no>
2024-03-22 09:19:52 +0100

    hm-katalog -> 0.1.34

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("no.nav.hjelpemidler:hm-katalog:0.1.21")
+            from("no.nav.hjelpemidler:hm-katalog:0.1.34")
         }
     }
 }
