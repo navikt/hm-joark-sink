a5ef9a3cbf1a021c53bd0bd962644ad503340641
Erik Eiesland <erik.eiesland@nav.no>
2024-06-11 13:39:33 +0200

    felles workflow for kodekvalitet

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("no.nav.hjelpemidler:hm-katalog:0.2.6")
+            from("no.nav.hjelpemidler:hm-katalog:0.2.9")
         }
     }
 }
