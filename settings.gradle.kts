fad55dc0bd116ea187cca72cef9235d32416502d
Erik Eiesland <erik.eiesland@nav.no>
2025-03-25 16:45:46 +0100

    ny hotlibs, gradle 8.13, fjerner branch fra deploy

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("no.nav.hjelpemidler:katalog:25.084.123801")
+            from("no.nav.hjelpemidler:katalog:25.084.130353")
         }
     }
 }
